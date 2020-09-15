05368ba91405aaa4d840d7fc01abe542bed21bf0
sharat86 <71310015+sharat86@users.noreply.github.com>
2020-09-15 07:12:26 +0100

    Add files via upload

---
+++ IfLoop.java
@@ -0,0 +1,19 @@
+package first;
+
+public class IfLoop {
+
+	public static void main(String[] args) {
+		int a=100;
+		int b=20;
+		
+		if(a<b) {
+			System.out.println("A is less than B");
+			System.out.println("You are in first loop");}
+			else {
+				System.out.println("A is greater than B");
+				System.out.println("you are in second loop");
+			}
+
+	}
+
+}
